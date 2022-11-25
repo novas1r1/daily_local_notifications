d2a74160892a1d9ac0e1e19ccbd6a9f558244ff2
Verena Zaiser <development@verena-zaiser.de>
2022-11-25 17:25:58 +0100

    initial commit

---
+++ lib/src/daily_local_notifications.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// {@template daily_local_notifications}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class DailyLocalNotifications {
+  /// {@macro daily_local_notifications}
+  const DailyLocalNotifications();
+}
---
+++ test/src/daily_local_notifications_test.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:daily_local_notifications/daily_local_notifications.dart';
+
+void main() {
+  group('DailyLocalNotifications', () {
+    test('can be instantiated', () {
+      expect(DailyLocalNotifications(), isNotNull);
+    });
+  });
+}
