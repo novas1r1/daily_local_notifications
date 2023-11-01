1a780615a50fa49be23849dd9344fd50d5a6d851
Verena Zaiser <development@verena-zaiser.de>
2023-11-01 12:16:50 +0100

    updated settings

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import flutter_local_notifications
 import flutter_timezone
-import shared_preferences_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
